d1c9cda0762d8bcb8194537b970fe6faf11c1f67
Your Name <you@example.com>
2022-05-17 12:59:48 +0300

    change

--- main.js
+++ main.js
@@ -21,37 +21,48 @@ const cards = [
   "images/12.jpg",
   "images/13.png"
 ];
-let a1=prompt("enter name1")
-let a2=prompt("enter name2")
-let i=0;
-let players=[{
-  player: a1,
-  score:0
-},
-{
-  player: a2,
-  score:0
-}
-]
+
+let f = document.getElementById('turn');
+
+//  let a1=prompt("enter name1")
+//  let a2=prompt("enter name2")
+let i = 0;
+
+let players = [];
 
 // const cardBack =
 let selectedCard = [];
 // const cardBack = "♥︎";
-let player1 = document.querySelector('#player1');
-let player2 = document.querySelector('#player2');
-player1.innerHTML= `${players[0].player} score: ${players[0].score}`
-player2.innerHTML= `${players[1].player} score: ${players[1].score}`
+// let player1 = document.querySelector('#player1');
+// let player2 = document.querySelector('#player2');
+// player1.innerHTML = `${players[0].player} score: ${players[0].score}`
+// player2.innerHTML = `${players[1].player} score: ${players[1].score}`
+// f.innerHTML = `turn : ${players[1].player}`
+let btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+  let player1 = document.querySelector('#player1');
+  let player2 = document.querySelector('#player2');
+  for (i = 31; i < 33; i++) {
+    let player = {
+      player: document.getElementById(`${i}`).value,
+      score: 0
+    }
+    players.push(player)
+  }
+  player1.textContent = `${players[0].player} score: ${players[0].score}`
+  player2.textContent = `${players[1].player} score: ${players[1].score}`
+  // GameStart();
+})
 
-// let score = 0;
-// player.textContent = score;
 
-// for(i of cards){
-//     let element=document.createElement("div");
-//     element.innerHTML=i;
-//     const board=document.getElementById("board");
+// function GameStart() {
+//   shufle(cards);
+//   const board = document.getElementById("board");
+//   for (i in cards) {
+//     const element = createCard(i);
 //     board.appendChild(element);
+//   }
 // }
-
 function shufle(arr) {
   arr = arr.sort(() => Math.random() - 0.5);
 }
@@ -60,8 +71,6 @@ function createCard(idx) {
   img.src = "images/image1.webp";
 
   const carEl = document.createElement("div");
-  // carEl.innerHTML=cardBack;
-
   carEl.appendChild(img);
   carEl.id = idx;
   carEl.className = "card";
@@ -70,12 +79,12 @@ function createCard(idx) {
 }
 function hideCard(div) {
   setTimeout(() => {
-     div.innerHTML = '';
-    div.classList.remove("fliped");
-    const img = document.createElement("img");
-    img.src = "images/image1.webp";
-    div.appendChild(img);
-  }, 700);
+    div.innerHTML = '';
+   div.classList.remove("fliped");
+   const img = document.createElement("img");
+   img.src = "images/image1.webp";
+   div.appendChild(img);
+ }, 700);
 }
 function onCardClick(event) {
   const div = event.currentTarget;
@@ -89,42 +98,40 @@ function onCardClick(event) {
     img.src = cards[cardId];
     div.classList.add("fliped");
     div.appendChild(img);
-    
-   
+
   }
-  // if(div.innerHTML == cardBack){
-  //     div.innerHTML = cards[cardId]
-  // }
-  
+
   if (selectedCard.length == 2) {
     if (cards[selectedCard[0]] != cards[selectedCard[1]]) {
-      if(i==0){
-        console.log("plaler 2")
+      if (i == 0) {
+        f.innerHTML = `turn : ${players[0].player}`
         i++;
-      }else{
-        i=0;
-        console.log("player 1")
+      } else {
+        i = 0;
+        f.innerHTML = `turn : ${players[1].player}`
       }
       const cardOne = document.getElementById(selectedCard[0]);
       hideCard(cardOne);
       const cardTwo = document.getElementById(selectedCard[1]);
       hideCard(cardTwo);
-    }  if(cards[selectedCard[0]] == cards[selectedCard[1]]&&(selectedCard[0]!=selectedCard[1] )){
-      players[i].score+=1;
-      if(i==0){
-        player1.innerHTML= `${players[0].player} score: ${players[0].score}`
+    } if (cards[selectedCard[0]] == cards[selectedCard[1]] && (selectedCard[0] != selectedCard[1])) {
+      players[i].score += 1;
+      if (i == 0) {
+        player1.innerHTML = `${players[0].player} score: ${players[0].score}`
       }
-      else{
-        player2.innerHTML= `${players[1].player} score: ${players[1].score}`
+      else {
+        player2.innerHTML = `${players[1].player} score: ${players[1].score}`
       }
 
     }
+
     selectedCard = [];
-      // score++;
-      // player.textContent = score;
-    }
-    
+
   }
+
+}
+
+
 
 shufle(cards);
 const board = document.getElementById("board");
